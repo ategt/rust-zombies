307e0e453a9bbb4e7dc39bdb7fb371676365fdc7
Adam Tegtmeier <ategtmei@gmail.com>
2020-01-28 23:54:17 -0500

    Simple guess the number game, sans random numbers.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 use std::io;
-use rand::Rng;
+//use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand:thread_rng().gen_range(1, 101);
+    //let secret_number = rand:thread_rng().gen_range(1, 101);
+    let secret_number = 7;
 
     println!("The Secret Number is {}", secret_number);
 
-    print!("Please Guess a Number: ");
+    println!("Please Guess a Number: ");
 
     let mut guess = String::new();
 
-    io.stdin().read_line(&mut guess)
+    io::stdin().read_line(&mut guess)
     	.expect("Failure to Read Line!");
 
 	println!("\nYou Guessed {}", guess);
