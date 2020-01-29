d421e8144695ba00f28d60fe2250d24eed5a38f5
Adam Tegtmeier <ategtmei@gmail.com>
2020-01-28 23:48:12 -0500

    Updated cargo toml file to include random number library.  When I have internet, run 'cargo build' and 'cargo update'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand:thread_rng().gen_range(1, 101);
+
+    println!("The Secret Number is {}", secret_number);
+
+    print!("Please Guess a Number: ");
+
+    let mut guess = String::new();
+
+    io.stdin().read_line(&mut guess)
+    	.expect("Failure to Read Line!");
+
+	println!("\nYou Guessed {}", guess);
 }
